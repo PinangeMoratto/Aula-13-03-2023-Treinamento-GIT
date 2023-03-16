64d6f86b1aad89655c795b43a5f3db24a629eba3
Samuel Moratto <ti.samuel.senac@hotmail.com>
2023-03-16 19:29:59 -0300

    Inclusão do pacote domain com a classe Usuario.java

---
+++ src/domain/Usuario.java
@@ -0,0 +1,33 @@
+public class Usuario {
+
+    private String nome;
+    private Integer id;
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
